567bef54c5e3a6fd2ef9d76b169680fe8f5522bc
LiiNi-coder <dlsgudrpdla@knu.ac.kr>
2024-02-08 05:23:04 +0900

    [FINISH] "babel과 webpack 설정하기"

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -16,8 +16,8 @@ const config: Configuration = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: !isDevelopment ? 'hidden-source-map' : 'eval',
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'], //바벨이 바꿔줄 확장자 목록
+    alias: { //tsconfig는 소스코드 타이핑중에 검사, 이 웹펙이 진짜 ts로 바꿔줄때 경로문제해결해줌 그래서 총 2번해줘야함
       '@hooks': path.resolve(__dirname, 'hooks'),
       '@components': path.resolve(__dirname, 'components'),
       '@layouts': path.resolve(__dirname, 'layouts'),
@@ -36,9 +36,10 @@ const config: Configuration = {
         loader: 'babel-loader',
         options: {
           presets: [
-            [
+            [//이렇게 3가지 설정해줘야지 IE10에서도 돌아가도록 해줌
               '@babel/preset-env',
               {
+                //브라우저를 명시해주면 그 브라우저를 지원하도록 설정을 가미해준다
                 targets: { browsers: ['IE 10'] },
                 debug: isDevelopment,
               },
@@ -54,7 +55,7 @@ const config: Configuration = {
         },
         exclude: path.join(__dirname, 'node_modules'),
       },
-      {
+      {//css파일을 loader들이 js로 바꿔줌
         test: /\.css?$/,
         use: ['style-loader', 'css-loader'],
       },
@@ -67,9 +68,11 @@ const config: Configuration = {
       //   files: "./src/**/*",
       // },
     }),
+    //백엔드(정확히는 node런타임)의 환경변수에 프론트엔드도 접근하도록 해주는 명령
     new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
   ],
   output: {
+    //위에서 쭈욱 명령해준대로 나오는 아웃풋 설정
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/dist/',
